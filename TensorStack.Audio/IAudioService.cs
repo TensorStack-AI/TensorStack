4b50c6fcfcccb9d87b125011151f8b8045f17a43
sa_ddam213 <sa_ddam213@live.com>
2025-10-03 16:06:17 +1300

    Add IAudioService

---
+++ TensorStack.Audio/IAudioService.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) TensorStack. All rights reserved.
+// Licensed under the Apache 2.0 License.
+using System.Threading.Tasks;
+
+namespace TensorStack.Audio
+{
+    public interface IAudioService
+    {
+        Task<AudioInfo> GetAudioInfoAsync(string filename);
+    }
+}
