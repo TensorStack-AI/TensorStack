221748cbb0d822c3cd64e84ad3b6a9f4b7614896
sa_ddam213 <sa_ddam213@live.com>
2025-11-24 18:23:13 +1300

    Supertonic configuration

---
+++ TensorStack.TextGeneration/Pipelines/Supertonic/SupertonicPipeline.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TensorStack.Common.Pipeline;
+using TensorStack.Common.Tensor;
+using TensorStack.TextGeneration.Common;
+
+namespace TensorStack.TextGeneration.Pipelines.Supertonic
+{
+    public class SupertonicPipeline : IPipeline<AudioTensor, SupertonicOptions, GenerateProgress>
+    {
+        public Task LoadAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task UnloadAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<AudioTensor> RunAsync(SupertonicOptions options, IProgress<GenerateProgress> progressCallback = null, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
