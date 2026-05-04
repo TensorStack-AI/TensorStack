77f7591d0acf1fcb035b64e54f4fa315c82ef39f
sa_ddam213 <sa_ddam213@live.com>
2026-05-04 15:44:15 +1200

    Nuget Updates

---
+++ TensorStack.Common/MediaType.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) TensorStack. All rights reserved.
+// Licensed under the Apache 2.0 License.
+namespace TensorStack.Common
+{
+    public enum MediaType
+    {
+        Image = 0,
+        Video = 1,
+        Audio = 2,
+        Text = 3
+    }
+}
